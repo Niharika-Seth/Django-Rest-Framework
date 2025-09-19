e97a5b62ed0a989f873fbe2235ee4fe3f6bb54e9
Niharika-Seth <niharikaseth1112@gmail.com>
2025-09-19 14:20:13 +0530

    Created views in views.py

--- gym_backend/gymapi/views.py
+++ gym_backend/gymapi/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
-
 # Create your views here.
+
+from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Plan, Trainer, Member
+from .serializers import PlanSerializer, TrainerSerializer, MemberSerializer
+
+class PlanViewSet(viewsets.ModelViewSet):
+    queryset = Plan.objects.all()
+    serializer_class = PlanSerializer
+
+class TrainerViewSet(viewsets.ModelViewSet):
+    queryset = Trainer.objects.all()
+    serializer_class = TrainerSerializer
+
+class MemberViewSet(viewsets.ModelViewSet):
+    queryset = Member.objects.all()
+    serializer_class = MemberSerializer
+
+
+
