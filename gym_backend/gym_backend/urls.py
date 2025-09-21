4fd041959cc376b762e27c6a8244092f6e65c1e7
Niharika-Seth <niharikaseth1112@gmail.com>
2025-09-21 20:43:42 +0530

    Registered models in admin.py file

--- gym_backend/gym_backend/urls.py
+++ gym_backend/gym_backend/urls.py
@@ -20,7 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('gymapi.urls')),   # API endpoints
-    path('', include('gymapp.urls')),       # Frontend
 ]
 
 
--- gym_backend/gymapi/admin.py
+++ gym_backend/gymapi/admin.py
@@ -1,3 +1,7 @@
-from django.contrib import admin
-
 # Register your models here.
+from django.contrib import admin
+from .models import Member, Plan, Trainer
+
+admin.site.register(Member)
+admin.site.register(Plan)
+admin.site.register(Trainer)
