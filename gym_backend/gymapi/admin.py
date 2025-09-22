6a19b97ec07167f07cd225aaacf7473581ef188c
Niharika-Seth <niharikaseth1112@gmail.com>
2025-09-22 22:17:57 +0530

    Updated admin, database and views

--- gym_backend/gymapi/admin.py
+++ gym_backend/gymapi/admin.py
@@ -1,7 +1,8 @@
 # Register your models here.
 from django.contrib import admin
-from .models import Member, Plan, Trainer
+from .models import Plan, Trainer, Member
 
-admin.site.register(Member)
 admin.site.register(Plan)
 admin.site.register(Trainer)
+admin.site.register(Member)
+
