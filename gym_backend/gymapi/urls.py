1753b57e3afd7bdeabffedcdd400cc514f92a4b4
Niharika-Seth <niharikaseth1112@gmail.com>
2025-09-25 23:29:01 +0530

    Added Token Authentication endpoint path to urls.py of gymapi

--- gym_backend/gymapi/urls.py
+++ gym_backend/gymapi/urls.py
@@ -7,8 +7,20 @@ router.register(r'plans', PlanViewSet)
 router.register(r'trainers', TrainerViewSet)
 router.register(r'members', MemberViewSet)
 
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
+from .views import PlanViewSet, TrainerViewSet, MemberViewSet
+
+router = DefaultRouter()
+router.register(r'plans', PlanViewSet)
+router.register(r'trainers', TrainerViewSet)
+router.register(r'members', MemberViewSet)
+
 urlpatterns = [
-    path('', include(router.urls)),
+    path("", include(router.urls)),
+    path("token-auth/", obtain_auth_token, name="api_token_auth"),  #  token endpoint
 ]
+
 
 
